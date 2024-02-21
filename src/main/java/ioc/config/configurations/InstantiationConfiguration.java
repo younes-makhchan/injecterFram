475d601b83646bafd6b756abefe52e2b4bb50468
Younes Makhchan <younessmakhchane@gmail.com>
2024-02-21 08:17:54 +0100

    fixing a bug

--- src/main/java/ioc/config/configurations/InstantiationConfiguration.java
+++ src/main/java/ioc/config/configurations/InstantiationConfiguration.java
@@ -6,9 +6,9 @@ import ioc.constants.Constants;
 
 public class InstantiationConfiguration extends BaseSubConfiguration {
     private  int maximumNumberOfiterations;
-    protected InstantiationConfiguration(InjectorConfiguration parentConfig,) {
+    protected InstantiationConfiguration(InjectorConfiguration parentConfig) {
         super(parentConfig);
-        this.maximumNumberOfiterations= Constants.MAX_NUMBER_OF_INSTANTIATIONS
+        this.maximumNumberOfiterations= Constants.MAX_NUMBER_OF_INSTANTIATIONS;
     }
 
     public  InstantiationConfiguration setMaximumNumberofAllowedIteration(int num){
